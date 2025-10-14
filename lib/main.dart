ae8783bade1b5aff09d62c9d0805b4aa260faccc
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-15 00:19:14 +0300

    add navigation logic to splash view

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruit_hub/core/helper_functions/on_generate_routes.dart';
 import 'package:fruit_hub/features/splash/presentation/views/splash_view.dart';
 
+import 'core/services/shared_preferences_singleton.dart';
 import 'generated/l10n.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await PrefsSingleton.init();
   runApp(const FruitHub());
 }
 
