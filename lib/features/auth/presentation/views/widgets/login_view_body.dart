e4db2dbc5ed69a20bd23ae4f62a1e724893ac62a
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-15 21:09:09 +0300

    finish login ui part 4

--- lib/features/auth/presentation/views/widgets/login_view_body.dart
+++ lib/features/auth/presentation/views/widgets/login_view_body.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/utils/app_colors.dart';
+import 'package:fruit_hub/core/widgets/custom_button.dart';
 import 'package:fruit_hub/core/widgets/custom_text_field.dart';
 
 import '../../../../../constants.dart';
+import '../../../../../core/utils/app_text_styles.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
       child: SingleChildScrollView(
         child: Column(
           children: <Widget>[
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
-            CustomTextField(
+            const CustomTextField(
               hintText: 'البريد الإلكتروني',
               textInputType: TextInputType.emailAddress,
             ),
-            SizedBox(height: 16),
-            CustomTextField(
+            const SizedBox(height: 16),
+            const CustomTextField(
               hintText: 'كلمة المرور',
               textInputType: TextInputType.visiblePassword,
               suffixIcon: Icon(
@@ -28,6 +31,43 @@ class LoginViewBody extends StatelessWidget {
                 color: Color(0xffC9CECF),
               ),
             ),
+            const SizedBox(
+              height: 16,
+            ),
+            Align(
+              alignment: AlignmentDirectional.centerEnd,
+              child: Text(
+                'نسيت كلمة المرور؟',
+                style: TextStyles.semiBold13.copyWith(
+                  color: AppColors.lightPrimaryColor,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 33,
+            ),
+            CustomButtons(
+              text: 'تسجيل دخول',
+              onPressed: () {},
+            ),
+             const SizedBox(
+              height: 33,
+            ),
+            Text.rich(
+              TextSpan(
+                text: 'لا تمتلك حساب؟',
+                style: TextStyles.semiBold16.copyWith(
+                  color: const Color(0xff949D9E),
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' قم بإنشاء حساب',
+                    style: TextStyles.semiBold13.copyWith(
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                ],
+            ))
           ],
         ),
       ),
