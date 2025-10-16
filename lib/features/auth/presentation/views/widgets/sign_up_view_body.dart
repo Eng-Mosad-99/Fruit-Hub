031fdba88a943c956e4cdfffdc9a671893d22d06
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-16 12:38:29 +0300

    finish sign up ui part 2

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,10 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:fruit_hub/core/widgets/custom_text_field.dart';
+import '../../../../../constants.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextField(
+              hintText: 'الاسم كامل',
+              textInputType: TextInputType.name,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'البريد الإلكتروني',
+              textInputType: TextInputType.emailAddress,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'كلمة المرور',
+              textInputType: TextInputType.visiblePassword,
+              suffixIcon: Icon(
+                Icons.remove_red_eye,
+                color: Color(0xffC9CECF),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
