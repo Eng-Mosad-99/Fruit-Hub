dd1b3b4f24d49aff6985bf5a0725f52623c8bcf2
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-18 15:35:27 +0300

    add sign in facebook package

--- lib/core/services/firebase_auth_service.dart
+++ lib/core/services/firebase_auth_service.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:fruit_hub/core/errors/exceptions.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
