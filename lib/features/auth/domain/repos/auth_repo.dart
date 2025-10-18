1927e5bb11f000b3db431756e9286c483f7833ad
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2025-10-18 15:02:57 +0300

    finish create sign in with google

--- lib/features/auth/domain/repos/auth_repo.dart
+++ lib/features/auth/domain/repos/auth_repo.dart
@@ -3,13 +3,14 @@ import 'package:fruit_hub/core/errors/failures.dart';
 import 'package:fruit_hub/features/auth/domain/enitites/user_entity.dart';
 
 abstract class AuthRepo {
-  Future<Either<Failure,UserEntity>> createUserWithEmailAndPassword({
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
     required String email,
     required String password,
     required String name,
   });
-  Future<Either<Failure,UserEntity>> signInWithEmailAndPassword({
+  Future<Either<Failure, UserEntity>> signInWithEmailAndPassword({
     required String email,
     required String password,
   });
+  Future<Either<Failure, UserEntity>> signInWithGoogle();
 }
