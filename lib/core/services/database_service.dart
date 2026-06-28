e43202a5a3ac654ed03b3ff1230a7899f2239d0f
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-28 08:15:12 -0400

    implement fetch user data part 2

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,11 +1,9 @@
-import 'package:fruits_ecommerce_app/features/auth/domain/entity/user_entity.dart';
-
 abstract class DatabaseService {
   Future<void> addData({
     required String path,
     required Map<String, dynamic> data,
   });
 
-  Future<UserEntity> getUserData({required String path , required String uID});
+  Future<Map<String , dynamic>> getData({required String path , required String documentID});
 }
 
--- lib/core/services/firestore_service.dart
+++ lib/core/services/firestore_service.dart
@@ -1,7 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fruits_ecommerce_app/features/auth/data/models/user_model.dart';
-import 'package:fruits_ecommerce_app/features/auth/domain/entity/user_entity.dart';
-
 import 'database_service.dart';
 
 class FirestoreService implements DatabaseService {
@@ -14,13 +11,10 @@ class FirestoreService implements DatabaseService {
   }) async {
     await firestore.collection(path).add(data);
   }
-
+  
   @override
-  Future<UserEntity> getUserData({
-    required String path,
-    required String uID,
-  }) async {
-    var data = await firestore.collection(path).doc(uID).get();
-    return UserModel.fromJson(data.data() as Map<String, dynamic>);
+  Future<Map<String, dynamic>> getData({required String path, required String documentID}) async{
+   var data= await firestore.collection(path).doc(documentID).get();
+   return data.data() as Map<String, dynamic>;
   }
 }
